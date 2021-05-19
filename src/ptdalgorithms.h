becf1c441e868cd3cbd688e1ddbe22cd3af3cf16
Tobias Røikjer <tobias.roikjer@post.au.dk>
2021-05-19 21:15:56 +0200

    cc

---
+++ src/ptdalgorithms.h
@@ -0,0 +1,12 @@
+#define PTD_RCPP 1
+#include <Rcpp.h>
+
+#include "c/ptdalgorithms.h"
+#include "cpp/ptdalgorithmscpp.h"
+
+using namespace Rcpp;
+using namespace ptdalgorithms;
+
+class Graph {
+  
+};
